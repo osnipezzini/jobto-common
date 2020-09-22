4f3903772c6671e16bd61a4c617bf482fd684db4
Osni Pezzini Junior <osni.pezzini@gmail.com>
2020-09-22 01:45:51 -0300

    Added a helper to get current person age
     - Added Person and User models

---
+++ Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobTo.Common.Models
+{
+    public class Person
+    {
+        [Key]
+        public long Grid { get; set; }
+        public string Doc { get; set; }
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public string Type { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public virtual IEnumerable<User> Users { get; set; }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace JobTo.Common.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public long EmployeeId { get; set; }
+        public Person Employee { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
+    }
+}
