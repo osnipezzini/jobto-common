d181210088dca3394fa4c725a566b81b434cda0b
Osni Pezzini Junior <osni.pezzini@gmail.com>
2020-09-23 04:16:25 -0300

    Created settings base and crypt helper

---
+++ Helpers/Crypt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JobTo.Common.Helpers
+{
+    public static class Crypt
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        public static string Decrypt(string value)
+        {
+            try
+            {
+                byte[] data = Convert.FromBase64String(value); // decrypt the incrypted text
+                using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                {
+                    byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(Settings.Secret));
+                    using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                    {
+                        ICryptoTransform transform = tripDes.CreateDecryptor();
+                        byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                        return UTF8Encoding.UTF8.GetString(results);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                Logger.Error(exc);
+            }
+            return value;
+        }
+        public static string Encrypt(string value)
+        {
+            try
+            {
+                byte[] data = UTF8Encoding.UTF8.GetBytes(value);
+                using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                {
+                    byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(Settings.Secret));
+                    using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                    {
+                        ICryptoTransform transform = tripDes.CreateEncryptor();
+                        byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                        return Convert.ToBase64String(results, 0, results.Length);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                Logger.Error(exc);
+            }
+            return value;
+
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace JobTo.Common
+{
+    public static class Settings
+    {
+        public static string Secret = "9695aec3e936f69ae420feb32e2e69a3";
+    }
+}
